094d59d688289836dae69a64a463040004fd85cd
lmg <lmg@edu.unifor.br>
2023-10-04 20:35:18 -0300

    versão 3

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req,res){
-    res.send("<div style = 'display:flex;flex-direction: column;align-items: center; justify-content:center'><h1 style ='padding=2px;'>Bem</h1><h1 style ='padding=2px;'>Vindo</h1><h1 style ='padding=2px;'>Ao</h1><h1 style ='padding=2px;'>Meu</h1><h1 style ='padding=2px;'>Site!</h1><a href='/produtos'>Lista de produtos</a>")
+    res.send("<div style = 'display:flex;flex-direction: row;align-items: center; justify-content:center'><h1 style ='padding=2px;'>Bem</h1><h1 style ='padding=2px;'>Vindo</h1><h1 style ='padding=2px;'>Ao</h1><h1 style ='padding=2px;'>Meu</h1><h1 style ='padding=2px;'>Site!</h1><a href='/produtos'>Lista de produtos</a>")
 })
 
 app.get("/produtos", function(req,res){
-    res.send("<div style = 'display:flex;flex-direction: column;align-items: center; justify-content:center'><h1 style ='padding=2px;'>Lista</h1><h1 style ='padding=2px;'>De</h1><h1 style ='padding=2px;'>Produtos</h1><a href='/'>Bem vindo ao meu site!</a>")
+    res.send("<div style = 'display:flex;flex-direction: row;align-items: center; justify-content:center'><h1 style ='padding=2px;'>Lista</h1><h1 style ='padding=2px;'>De</h1><h1 style ='padding=2px;'>Produtos</h1><a href='/'>Bem vindo ao meu site!</a>")
 })
 
 app.listen(4000,function(erro){
